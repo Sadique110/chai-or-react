e10809cee8ff83a6752e8829dc7257ea7b809fad
Mohammad sadique <mohammadsadique110786@gmail.com>
2024-10-21 00:26:10 +0530

    react js deep learning

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,11 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client' 
 import App from './App.jsx'
 import Chai from './chai.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-    <Chai />
-  </>,
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>Custom react | Chai</h1>
+//     </div>
+//   )
+  
+// }
+// const ReactElement ={
+//   type: 'a',
+//   props: {href: 'https://google.com',
+//           target: '_blank'
+//         },
+//   childrent: 'click me to visit google'
+// }
+      
+const anotherElement = React.createElement(
+  <a href='https://google.com' target ='_blank'>visit google </a>)
+
+
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visit google')
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
 )
